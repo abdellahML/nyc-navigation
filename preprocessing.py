26de4d389c37e6f16aec63c2f1ed1dc587cd4371
Ebu <ebu@iBoo.lan>
2021-01-07 14:42:18 +0100

    little change on ny_map_osmnx, as class name, def etc...

--- preprocessing.py
+++ preprocessing.py
@@ -7,15 +7,15 @@ class Preprocessing:
     """In this class we will prepare our dataset before tracing our path on ny_map_osmnx.py
     the main objective is to have a data group by street name and each street with a danger weight"""
 
-    def import_csv_file(self, csv_file: str) --> Type(dataframe):
-        """This function will import our data as dataframe"""
+    def import_csv_file(self, csv_file: str):
+        """This function will import our data as dataframe and return a variable dataframe"""
 
         crash = pd.read_csv(csv_file)
         return crash
     
-    def add_danger_weight(self, origin: Type(float, float), destination: Type(float, float), crash: Type(dataframe)) --> Type(dataframe):
-        """This function will create a  new colonne in our dataframe which is the weight of danger for each street,
-        it will be based on the number of accident by street"""
+    def add_danger_weight(self, origin: Type(float, float), destination: Type(float, float), crash):
+        """This function will create a  new column in our dataframe which is the weight of danger for each street,
+        it will be based on the number of accident by street and will return the changed dataframe"""
 
         crash['danger_weight'] = crash['number_of_accident']
 
