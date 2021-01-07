dde16504dd0c2593c882f57c056bd7206c9401ca
Pierre Wasilewski <pierre.wasilewski.webdev.com>
2021-01-07 11:57:47 +0100

    Added flask routing
    for home and request form
    Need to create getSafest function

--- ny_map_osmnx.py
+++ ny_map_osmnx.py
@@ -9,10 +9,11 @@ import matplotlib.colors as colors
 place = 'New york city,New York, USA'
 G = ox.graph_from_place(place, network_type='drive')
 
+route = ox.shortest_path(G, orig, dest, weight='length')
+#fig, ax = ox.plot_graph_route(G, route, route_color='y', route_linewidth=6, node_size=0)
+
 # convert your MultiDiGraph to an undirected MultiGraph
 #M = ox.get_undirected(G)
-
 # convert your MultiDiGraph to a DiGraph without parallel edges
 #D = ox.get_digraph(G)
-
 #fig, ax = ox.plot_graph(G,node_size=0, edge_linewidth=0.5)
