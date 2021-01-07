c9cfd01f35204dc80d5d1e401fb047c0df745e5c
Pierre Wasilewski <pierre.wasilewski.webdev.com>
2021-01-07 11:23:04 +0100

    Added home.html

--- ny_map_osmnx.py
+++ ny_map_osmnx.py
@@ -5,14 +5,14 @@ import matplotlib.cm as cm
 import matplotlib.colors as colors
 
 
-# download/model a street network for some city then visualize it
-G = ox.graph_from_place('New york city,New York, USA', network_type='all')
 
+place = 'New york city,New York, USA'
+G = ox.graph_from_place(place, network_type='drive')
 
 # convert your MultiDiGraph to an undirected MultiGraph
-M = ox.get_undirected(G)
+#M = ox.get_undirected(G)
 
 # convert your MultiDiGraph to a DiGraph without parallel edges
-D = ox.get_digraph(G)
+#D = ox.get_digraph(G)
 
-fig, ax = ox.plot_graph(G,node_size=0, edge_linewidth=0.5)
+#fig, ax = ox.plot_graph(G,node_size=0, edge_linewidth=0.5)
