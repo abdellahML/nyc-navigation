dc9dc0ff7fbfd1ec1a0cf72a7789bb3773330c06
abdellah <abdellah.ghilbzouri@gmail.com>
2021-01-08 09:20:48 +0100

    safest_way fonction

--- ny_map_osmnx.py
+++ ny_map_osmnx.py
@@ -27,3 +27,9 @@ class NYMapOSMnx:
     # convert your MultiDiGraph to a DiGraph without parallel edges
     #D = ox.get_digraph(G)
     #fig, ax = ox.plot_graph(G,node_size=0, edge_linewidth=0.5)
+
+    def safest_way(origin,destination):                                                                 # select safest route
+        route = ox.shortest_path(G, origin, destination, weight='danger_weight')
+        fig, ax = ox.plot_graph_route(G, route, route_color='y', route_linewidth=6, node_size=0)        # plot the safest road
+        route_risk = int(sum(ox.utils_graph.get_route_edge_attributes(G, route, 'danger_weight')))      # print the risk on this road
+        print('The risk on this Route is ', route_risk, ' accidents per year')
