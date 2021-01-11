96bf14b55f687d8e78f18797765b2b6ebb731272
abdellah <abdellah.ghilbzouri@gmail.com>
2021-01-11 14:37:40 +0100

    changes

--- test.py
+++ test.py
@@ -8,14 +8,15 @@ import matplotlib.colors as colors
 place = 'New york city,New York, USA'
 G = ox.graph_from_place(place, network_type='drive')
 
-def safest_way(origin,destination):                                                                 # select safest route
+def safest_way(origin, destination):                                                                 # select safest route
     route = ox.shortest_path(G, origin, destination, weight='danger_weight')
     fig, ax = ox.plot_graph_route(G, route, route_color='y', route_linewidth=6, node_size=0)        # plot the safest road
     route_risk = int(sum(ox.utils_graph.get_route_edge_attributes(G, route, 'danger_weight')))      # print the risk on this road
     txt1 = 'The risk on this Route is ' 
     txt2 =  ' accidents per year'
-    return fig,txt1,txt2,route_risk
-def shortest_safest(origin,destination):
+    return fig, txt1, txt2, route_risk
+
+def shortest_safest(origin, destination):
     route1 = ox.shortest_path(G, origin, destination, weight='danger_weight') #safest road
     route2 = ox.shortest_path(G, origin, destination, weight='length')        #shortest road
     fig, ax = ox.plot_graph_routes(G, routes=[route1, route2], route_colors=['r', 'y'],route_linewidth=6, node_size=0)
